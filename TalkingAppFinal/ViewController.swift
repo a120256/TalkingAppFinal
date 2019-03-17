cc4921261cf3c21c7d22243cd4aab80ec5b357bc
帥氣阿剛 <alex_wang@tinggangwangde-macbook-pro.local>
2019-03-17 17:37:50 +0800

    Initial Commit

---
+++ TalkingAppFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalkingAppFinal
+//
+//  Created by 帥氣阿剛 on 2019/3/17.
+//  Copyright © 2019 帥氣阿剛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
